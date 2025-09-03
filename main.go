be579151a35d3120ead736c8d87648b1e3c8f72c
SeanPiedmonte <seanpiedmonte@gmail.com>
2025-09-03 14:17:32 -0500

    start on financial doc processing

--- main.go
+++ main.go
@@ -3,7 +3,24 @@ package main
 import (
 	"log"
 	"net/http"
+	"bufio"
+	"os"
+	"fmt"
 )
+
+type Transaction struct {
+	amount int
+	desc   string
+}
+
+func process_transactions(filename string) {
+	file, err := os.OpenFile(filename, os.O_RDWR, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(file)
+	fmt.Println("%v", scanner)
+}
 
 func main() {
 	fs := http.FileServer(http.Dir("./ui/dist"))
